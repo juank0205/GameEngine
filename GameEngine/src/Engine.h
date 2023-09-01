00061dca3edfb5808ed04a1d24d77b77b8b04273
juank0205 <jkamiloeche@outlook.com>
2023-08-31 22:18:39 -0500

    Added logging

--- GameEngine/src/Engine.h
+++ GameEngine/src/Engine.h
@@ -3,6 +3,7 @@
 //For use by hazel applications
 
 #include "Engine/Application.h"
+#include "Engine/Log.h"
 
 // Entry point
 #include "Engine/EntryPoint.h"
--- GameEngine/src/Engine/EntryPoint.h
+++ GameEngine/src/Engine/EntryPoint.h
@@ -7,8 +7,12 @@ extern Engine::Application* Engine::CreateApplication();
 
 void main(int argc, char** argv)
 {
+
+	Engine::Log::Init();
+	GE_CORE_WARN("Initialized Log!");
+	GE_CLIENT_INFO("LAMO");
+
 	auto app = Engine::CreateApplication();
-	std::cout << "Game Engine";
 	app->Run();
 	delete app;
 }
